c59f0dc08dfbbbb2721c115b951f6c34ca6a909e
ohjuyoungg <ejrm12@naver.com>
2023-11-10 16:43:46 +0900

    feat: 이벤트 컨트롤러 클래스 생성

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -1,2 +1,15 @@
-package christmas.controller;public class EventController {
+package christmas.controller;
+
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class EventController {
+
+    OutputView outputView = new OutputView();
+    InputView inputView = new InputView();
+
+    public void eventStart() {
+        inputView.printEventStart();
+
+    }
 }
