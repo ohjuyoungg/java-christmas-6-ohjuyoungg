3159dcff4ed1bc409aed60697ba6d8a83030ea26
ohjuyoungg <ejrm12@naver.com>
2023-11-10 16:41:40 +0900

    feat: 이벤트를 시작하는 문구를 출력하는 메소드 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,2 +1,9 @@
-package christmas.view;public class InputView {
+package christmas.view;
+
+import christmas.utils.constants.InputComment;
+
+public class InputView {
+    public void printEventStart() {
+        System.out.println(InputComment.EVENT_PLANNER_STATIONERY_START.getInputComment());
+    }
 }
