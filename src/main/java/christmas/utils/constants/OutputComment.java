0ac1994788b05d123d4bc2a3380970ec9e97c5a2
ohjuyoungg <ejrm12@naver.com>
2023-11-10 16:26:30 +0900

    feat: OutputComment enum 클래스 작성

--- src/main/java/christmas/utils/constants/OutputComment.java
+++ src/main/java/christmas/utils/constants/OutputComment.java
@@ -1,2 +1,23 @@
-package christmas.utils.constants;public class OutputComment {
+package christmas.utils.constants;
+
+public enum OutputComment {
+    ORDER_MENU("<주문 메뉴>"),
+    TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT("<할인 전 총주문 금액>"),
+    GIVEAWAY_MENU("<증정 메뉴>"),
+    BENEFIT_DETAILS("<총혜택 금액>"),
+    ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT("<할인 후 예상 결제 금액>"),
+    DECEMBER_EVENT_BADGE("<12월 이벤트 배지>"),
+    KRW("원"),
+    COUNT("개"),
+    NOTHING("없음");
+
+    private final String outputComment;
+
+    OutputComment(String outputComment) {
+        this.outputComment = outputComment;
+    }
+
+    public String getOutputComment() {
+        return outputComment;
+    }
 }
