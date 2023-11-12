03f36a3e9dd99b4e0b9907844a624a7db363d3d8
ohjuyoungg <ejrm12@naver.com>
2023-11-13 01:33:59 +0900

    feat: 사용자에게 주문할 메뉴와 개수를 받는 메소드 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -3,8 +3,27 @@ package christmas.view;
 import camp.nextstep.edu.missionutils.Console;
 import christmas.utils.constants.InputComment;
 import christmas.utils.validator.DateValidator;
+import christmas.utils.validator.MenuValidator;
 
 public class InputView {
+
+    public static int userDate() {
+        while (true) {
+            String input = Console.readLine();
+            if (DateValidator.validateDateErrors(input)) {
+                return DateValidator.userInput(input);
+            }
+        }
+    }
+
+    public static String userMenu() {
+        while (true) {
+            String input = Console.readLine();
+            if (MenuValidator.validateMenuErrors(input)) {
+                return MenuValidator.userInput(input);
+            }
+        }
+    }
 
     public void EventStart() {
         System.out.println(InputComment.EVENT_PLANNER_STATIONERY_START.getInputComment());
@@ -14,12 +33,7 @@ public class InputView {
         System.out.println(InputComment.EXPECTED_DATE_OF_VISIT_TO_RESTAURANT.getInputComment());
     }
 
-    public static int userDate() {
-        while (true) {
-            String input = Console.readLine();
-            if (DateValidator.validateDateErrors(input)) {
-                return DateValidator.parseInput(input);
-            }
-        }
+    public void orderMenuNumberOfMenus() {
+        System.out.println(InputComment.GET_THE_MENU_AND_QUANTITY_TO_ORDER.getInputComment());
     }
 }
