1f68738082c5703d217f984be058d6839b2ae75a
ohjuyoungg <ejrm12@naver.com>
2023-11-13 01:34:55 +0900

    feat: 메뉴이름 및 가격 관련 enum 클래스 구현

--- src/main/java/christmas/domain/menu/MenuInfo.java
+++ src/main/java/christmas/domain/menu/MenuInfo.java
@@ -1,2 +1,55 @@
-package christmas.domain.menu;public class MenuInfo {
+package christmas.domain.menu;
+
+public enum MenuInfo {
+    // APPETIZER
+    BUTTON_MUSHROOM_SOUP("양송이수프", 6000),
+    TAPAS("타파스", 5500),
+    CAESAR_SALAD("시저샐러드", 8000),
+
+    // MAIN
+    T_BONE_STEAK("티본스테이크", 55000),
+    BARBECUE_RIBS("바비큐립", 54000),
+    SEA_FOOD_PASTA("해산물파스타", 35000),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000),
+
+    // DESSERT
+    CHOCOLATE_CAKE("초코케이크", 15000),
+    ICE_CREAM("아이스크림", 5000),
+
+    // DRINKS
+    ZERO_COKE("제로콜라", 3000),
+    RED_WINE("레드와인", 60000),
+    CHAMPAGNE("샴페인", 25000);
+
+    private final String menuName;
+    private final int price;
+
+    MenuInfo(String menuName, int price) {
+        this.menuName = menuName;
+        this.price = price;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public static boolean isZeroCoke(String menuName) {
+        return "제로콜라".equals(menuName);
+    }
+
+    public static boolean isRedWine(String menuName) {
+        return "레드와인".equals(menuName);
+    }
+
+    public static boolean isChampagne(String menuName) {
+        return "샴페인".equals(menuName);
+    }
+
+    public static boolean isDrink(String menuName) {
+        return isZeroCoke(menuName) || isRedWine(menuName) || isChampagne(menuName);
+    }
 }
