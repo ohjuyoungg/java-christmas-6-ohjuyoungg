b3f273d1f7484717b8290056dd08c8bc7fff4b3a
ohjuyoungg <ejrm12@naver.com>
2023-11-13 01:33:27 +0900

    feat: domain 패키지 내에 Menu 클래스 생성

--- src/main/java/christmas/domain/menu/Menu.java
+++ src/main/java/christmas/domain/menu/Menu.java
@@ -1,2 +1,21 @@
-package christmas.domain.menu;public class Menu {
+package christmas.domain.menu;
+
+import christmas.domain.price.Price;
+
+public class Menu {
+    private final MenuInfo info;
+    private final Price price;
+
+    public Menu(MenuInfo info, Price price) {
+        this.info = info;
+        this.price = price;
+    }
+
+    public MenuInfo getInfo() {
+        return info;
+    }
+
+    public Price getPrice() {
+        return price;
+    }
 }
