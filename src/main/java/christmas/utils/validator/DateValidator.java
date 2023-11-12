94ff93cf01d5ec452a9ffcdd6f8c8b734e620ef9
ohjuyoungg <ejrm12@naver.com>
2023-11-13 01:30:06 +0900

    refactor: 접근제어자 메소드 리팩토링

--- src/main/java/christmas/utils/validator/DateValidator.java
+++ src/main/java/christmas/utils/validator/DateValidator.java
@@ -6,12 +6,12 @@ import christmas.view.InputView;
 public class DateValidator {
 
     private static void validateDate(String input) {
-        DateValidator.isBlank(input);
-        DateValidator.isString(input);
-        DateValidator.validateDateRange(Integer.parseInt(input));
+        isBlank(input);
+        isString(input);
+        validateDateRange(Integer.parseInt(input));
     }
 
-    public static int parseInput(String input) {
+    public static int userInput(String input) {
         try {
             return Integer.parseInt(input);
         } catch (IllegalArgumentException e) {
@@ -30,19 +30,19 @@ public class DateValidator {
         }
     }
 
-    public static void isBlank(String dateValue) {
+    private static void isBlank(String dateValue) {
         if (dateValue == null || dateValue.isBlank()) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_DATE.getExceptionMessage());
         }
     }
 
-    public static void validateDateRange(int dateValue) {
+    private static void validateDateRange(int dateValue) {
         if (dateValue > 31 || dateValue < 1) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_DATE.getExceptionMessage());
         }
     }
 
-    public static void isString(String dateValue) {
+    private static void isString(String dateValue) {
         if (!dateValue.matches("\\d+")) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_DATE.getExceptionMessage());
         }
