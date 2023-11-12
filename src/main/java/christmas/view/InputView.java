bcfc5f5545d69422a6af2543e478204fbb78ed33
ohjuyoungg <ejrm12@naver.com>
2023-11-12 13:30:27 +0900

    feat: 사용자 방문 날짜 입력 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,9 +1,25 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import christmas.utils.constants.InputComment;
+import christmas.utils.validator.DateValidator;
 
 public class InputView {
-    public void printEventStart() {
+
+    public void EventStart() {
         System.out.println(InputComment.EVENT_PLANNER_STATIONERY_START.getInputComment());
+    }
+
+    public void ExpectedVisitDate() {
+        System.out.println(InputComment.EXPECTED_DATE_OF_VISIT_TO_RESTAURANT.getInputComment());
+    }
+
+    public static int userDate() {
+        while (true) {
+            String input = Console.readLine();
+            if (DateValidator.validateDateErrors(input)) {
+                return DateValidator.parseInput(input);
+            }
+        }
     }
 }
