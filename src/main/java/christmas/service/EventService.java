687e83325523de1ac5b33a09a35b87700c43fc8f
ohjuyoungg <ejrm12@naver.com>
2023-11-13 20:05:44 +0900

    feat: 주문메뉴 출력 관련 서비스 클래스 생성 및 구현

--- src/main/java/christmas/service/EventService.java
+++ src/main/java/christmas/service/EventService.java
@@ -1,2 +1,27 @@
-package christmas.service;public class EventService {
+package christmas.service;
+
+import christmas.domain.order.Order;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class EventService {
+
+    public void printMenu(Order userOrder) {
+        List<String> orderedMenu = new ArrayList<>();
+
+        Map<String, Integer> orderItems = userOrder.getOrderItems();
+        for (Map.Entry<String, Integer> entry : orderItems.entrySet()) {
+            String menuName = entry.getKey();
+            int quantity = entry.getValue();
+
+            orderedMenu.add(String.format("%s %d개", menuName, quantity));
+        }
+        Collections.reverse(orderedMenu);
+        for (String menu : orderedMenu) {
+            System.out.println(menu);
+        }
+    }
 }
