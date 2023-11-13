67b59cc4979eceeb0106b4fba90c69948fe999e3
ohjuyoungg <ejrm12@naver.com>
2023-11-13 20:05:16 +0900

    feat: 주문메뉴 출력 관련 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,10 @@
 package christmas.view;
 
+import christmas.utils.constants.OutputComment;
+
 public class OutputView {
 
+   public void orderMenu() {
+       System.out.println(OutputComment.ORDER_MENU.getOutputComment());
+    }
 }
