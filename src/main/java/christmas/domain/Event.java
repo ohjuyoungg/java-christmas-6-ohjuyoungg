73c55ccb0bb23dfd41063df33756e6f89939966c
ohjuyoungg <ejrm12@naver.com>
2023-11-13 17:19:07 +0900

    feat: 도메인 패키지 내에 Event 클래스 구현

--- src/main/java/christmas/domain/Event.java
+++ src/main/java/christmas/domain/Event.java
@@ -1,2 +1,22 @@
-package christmas.domain;public class Event {
+package christmas.domain;
+
+import christmas.domain.date.Date;
+import christmas.domain.order.Order;
+
+public class Event {
+    private final Date visitDate;
+    private final Order order;
+
+    public Event(Date visitDate, Order order) {
+        this.visitDate = visitDate;
+        this.order = order;
+    }
+
+    public Date getVisitDate() {
+        return visitDate;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
 }
