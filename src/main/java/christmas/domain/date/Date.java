2cb086d473a29cb90b4e293e080fa8d70196bb4e
ohjuyoungg <ejrm12@naver.com>
2023-11-13 17:25:00 +0900

    feat: 도메인 패키지 내에 Date 클래스 구현

--- src/main/java/christmas/domain/date/Date.java
+++ src/main/java/christmas/domain/date/Date.java
@@ -1,2 +1,18 @@
-package christmas.domain.date;public class Date {
+package christmas.domain.date;
+
+public class Date {
+    private final int date;
+
+    public Date(int date) {
+        this.date = date;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(date);
+    }
 }
