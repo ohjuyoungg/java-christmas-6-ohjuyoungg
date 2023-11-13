d57704b3394fb79a02553be0c9fca6304152a45b
ohjuyoungg <ejrm12@naver.com>
2023-11-13 17:26:10 +0900

    feat: Order 클래스 내에 생성자 및 Map 컬렉션프레임워크 생성

--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -1,5 +1,20 @@
 package christmas.domain.order;
 
-public class Order {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Order {
+    private final Map<String, Integer> orderItems;
+
+    public Order() {
+        this.orderItems = new HashMap<>();
+    }
+
+    public void addOrderItem(String menu, int quantity) {
+        orderItems.put(menu, quantity);
+    }
+
+    public Map<String, Integer> getOrderItems() {
+        return new HashMap<>(orderItems);
+    }
 }
