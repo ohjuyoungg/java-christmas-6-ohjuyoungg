b8e5bdf11b0790f91429c8d10209aa28fa84da40
ohjuyoungg <ejrm12@naver.com>
2023-11-13 20:04:59 +0900

    feat: 주문메뉴 입력 관련 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,11 +1,13 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.domain.date.Date;
-import christmas.domain.menu.Menu;
+import christmas.domain.order.Order;
 import christmas.utils.constants.InputComment;
 import christmas.utils.validator.DateValidator;
 import christmas.utils.validator.MenuValidator;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class InputView {
 
@@ -44,5 +46,19 @@ public class InputView {
     public static void eventBenefitsPreview(int day) {
         String message = String.format(InputComment.EVENT_BENEFITS_PREVIEW.getInputComment(), day);
         System.out.println(message);
+        System.out.println();
+    }
+
+    public static void tokenizeAndAddToOrder(String eventMenu, Order userOrder) {
+        StringTokenizer tokenizer = new StringTokenizer(eventMenu, ",");
+        while (tokenizer.hasMoreTokens()) {
+            String menuToken = tokenizer.nextToken().trim();
+            List<String> menuInfo = Arrays.asList(menuToken.split("-"));
+            String menuName = menuInfo.get(0).trim();
+            int quantity = Integer.parseInt(menuInfo.get(1).trim());
+            userOrder.addOrder(menuName, quantity);
+        }
     }
 }
+
+
