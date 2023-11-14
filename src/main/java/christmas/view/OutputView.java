e500ce86d4e678609927ca8e376d5643f1e8b21e
ohjuyoungg <ejrm12@naver.com>
2023-11-15 04:38:12 +0900

    refactor: 혜택 내역 출력 역할에 맞게 코드 리팩토링

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,10 @@
 package christmas.view;
 
+import static christmas.domain.benefit.DiscountType.*;
+import static christmas.service.EventPlanner.MINIMUM_PRICE_FOR_GIVEAWAY;
+import static christmas.service.EventPlanner.df;
+
+import christmas.domain.benefit.Discount;
 import christmas.utils.constants.OutputComment;
 
 public class OutputView {
@@ -22,6 +27,27 @@ public class OutputView {
     public static void benefitDetails() {
         System.out.println();
         System.out.println(OutputComment.BENEFIT_DETAILS.getOutputComment());
+    }
+
+    public static void benefits(Discount discount, int beforeDiscountPrice) {
+        if (discount.getAmount(CHRISTMAS) > 0) {
+            System.out.println("크리스마스 디데이 할인: -" + df.format(discount.getAmount(CHRISTMAS)) + OutputComment.KRW.getOutputComment());
+        }
+        if (discount.getAmount(WEEKDAY) > 0) {
+            System.out.println("평일 할인: -" + df.format(discount.getAmount(WEEKDAY)) + OutputComment.KRW.getOutputComment());
+        }
+        if (discount.getAmount(WEEKEND) > 0) {
+            System.out.println("주말 할인: -" + df.format(discount.getAmount(WEEKEND)) + OutputComment.KRW.getOutputComment());
+        }
+        if (discount.getAmount(SPECIAL) > 0) {
+            System.out.println("특별 할인: -" + df.format(discount.getAmount(SPECIAL)) + OutputComment.KRW.getOutputComment());
+        }
+        if (beforeDiscountPrice >= MINIMUM_PRICE_FOR_GIVEAWAY) {
+            System.out.println("증정 이벤트: -25,000원");
+        }
+        if (discount.hasNoDiscount() && beforeDiscountPrice < MINIMUM_PRICE_FOR_GIVEAWAY) {
+            System.out.println(OutputComment.NOTHING.getOutputComment());
+        }
     }
 }
 
