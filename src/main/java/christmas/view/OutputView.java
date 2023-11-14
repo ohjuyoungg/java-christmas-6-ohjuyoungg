0a58c3918a09878f65b81915d8236488a4fdddfe
ohjuyoungg <ejrm12@naver.com>
2023-11-14 15:06:47 +0900

    feat: 할인 전 총주문 금액 출력하는 메소드 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -4,7 +4,23 @@ import christmas.utils.constants.OutputComment;
 
 public class OutputView {
 
-   public void orderMenu() {
-       System.out.println(OutputComment.ORDER_MENU.getOutputComment());
+    public static void orderMenu() {
+        System.out.println(OutputComment.ORDER_MENU.getOutputComment());
+    }
+
+    public static void beforeDiscount(int totalAmount) {
+        System.out.println();
+        System.out.println(OutputComment.TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT.getOutputComment());
+        System.out.printf("%,d원%n", totalAmount);
+    }
+
+    public static void giveawayMenu() {
+        System.out.println();
+        System.out.println(OutputComment.GIVEAWAY_MENU.getOutputComment());
+    }
+
+    public static void benefitDetails() {
+        System.out.println();
+        System.out.println(OutputComment.BENEFIT_DETAILS);
     }
 }
