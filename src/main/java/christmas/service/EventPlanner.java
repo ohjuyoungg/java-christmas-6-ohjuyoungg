d964c82ae199293f8b99f717d6386c8e55fdb168
ohjuyoungg <ejrm12@naver.com>
2023-11-15 01:32:15 +0900

    refactor: 상수값 설정으로 코드 리팩토링

--- src/main/java/christmas/service/EventPlanner.java
+++ src/main/java/christmas/service/EventPlanner.java
@@ -1,6 +1,7 @@
 package christmas.service;
 
 import christmas.domain.order.Order;
+import christmas.utils.constants.OutputComment;
 import java.text.DecimalFormat;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -9,6 +10,9 @@ public class EventPlanner {
 
     private static LocalDate dateUtils = LocalDate.of(2023, 12, 1);
     private static DecimalFormat df = new DecimalFormat("#,###");
+    private static final int ZERO = 0;
+    private static final int TWELVE_THOUSAND = 120000;
+    private static final int THOUSAND = 1000;
 
     public static void calculateBenefits(int eventDate, Order userOrder, int beforeDiscountPrice) {
 
@@ -19,30 +23,30 @@ public class EventPlanner {
         LocalDate dateUtils = LocalDate.of(2023, 12, eventDate);
         final int starDiscount = calculateStarDiscount(eventDate, dateUtils.getDayOfWeek());
 
-        if (christmasDiscount > 0) {
-            System.out.println("크리스마스 디데이 할인: -" + df.format(christmasDiscount) + "원");
+        if (christmasDiscount > ZERO) {
+            System.out.println("크리스마스 디데이 할인: -" + df.format(christmasDiscount) + OutputComment.KRW.getOutputComment());
         }
-        if (weekdayDiscount > 0) {
-            System.out.println("평일 할인: -" + df.format(weekdayDiscount) + "원");
+        if (weekdayDiscount > ZERO) {
+            System.out.println("평일 할인: -" + df.format(weekdayDiscount) + OutputComment.KRW.getOutputComment());
         }
-        if (weekendDiscount > 0) {
-            System.out.println("주말 할인: -" + df.format(weekendDiscount) + "원");
+        if (weekendDiscount > ZERO) {
+            System.out.println("주말 할인: -" + df.format(weekendDiscount) + OutputComment.KRW.getOutputComment());
         }
-        if (starDiscount > 0) {
-            System.out.println("특별 할인: -" + df.format(starDiscount) + "원");
+        if (starDiscount > ZERO) {
+            System.out.println("특별 할인: -" + df.format(starDiscount) + OutputComment.KRW.getOutputComment());
         }
-        if (beforeDiscountPrice >= 120000) {
+        if (beforeDiscountPrice >= TWELVE_THOUSAND) {
             System.out.println("증정 이벤트: -25,000원");
         }
-        if (christmasDiscount == 0 && weekdayDiscount == 0 && weekendDiscount == 0 && starDiscount == 0
-            && beforeDiscountPrice < 120000) {
-            System.out.println("없음");
+        if (christmasDiscount == ZERO && weekdayDiscount == ZERO && weekendDiscount == ZERO && starDiscount == ZERO
+            && beforeDiscountPrice < TWELVE_THOUSAND) {
+            System.out.println(OutputComment.NOTHING.getOutputComment());
         }
         calculateTotalBenefits(christmasDiscount, weekdayDiscount, weekendDiscount, starDiscount, beforeDiscountPrice);
         int benefits = christmasDiscount + weekdayDiscount + weekendDiscount + starDiscount;
         printAfterDiscountPrice(beforeDiscountPrice, benefits);
-        int benefit = christmasDiscount + weekdayDiscount + weekendDiscount + starDiscount + 250000;
-        calculateEventBadge(benefits);
+        int totalBenefits = christmasDiscount + weekdayDiscount + weekendDiscount + starDiscount + 25000;
+        calculateEventBadge(totalBenefits);
     }
 
 
@@ -51,43 +55,43 @@ public class EventPlanner {
         DecimalFormat df = new DecimalFormat("#,###");
         int benefits = christmasDiscount + weekdayDiscount + weekendDiscount + starDiscount;
 
-        if (benefits <= 0) {
+        if (benefits <= ZERO) {
             System.out.println();
-            System.out.println("<총혜택 금액>");
-            System.out.println(0 + "원");
+            System.out.println(OutputComment.TOTAL_BENEFIT_DETAILS.getOutputComment());
+            System.out.println(ZERO + OutputComment.KRW.getOutputComment());
         }
-        if (beforeDiscountPrice >= 120000) {
+        if (beforeDiscountPrice >= TWELVE_THOUSAND) {
             int total = benefits + 25000;
             System.out.println();
-            System.out.println("<총혜택 금액>");
-            System.out.println("-" + df.format(total) + "원");
-        } else if (beforeDiscountPrice < 120000 && benefits > 0) {
+            System.out.println(OutputComment.TOTAL_BENEFIT_DETAILS.getOutputComment());
+            System.out.println("-" + df.format(total) + OutputComment.KRW.getOutputComment());
+        } else if (beforeDiscountPrice < 120000 && benefits > ZERO) {
             System.out.println();
-            System.out.println("<총혜택 금액>");
-            System.out.println("-" + df.format(benefits) + "원");
+            System.out.println(OutputComment.TOTAL_BENEFIT_DETAILS.getOutputComment());
+            System.out.println("-" + df.format(benefits) + OutputComment.KRW.getOutputComment());
         }
     }
 
     public static void giveAway(int beforeDiscountPrice) {
-        if (beforeDiscountPrice >= 120000) {
+        if (beforeDiscountPrice >= TWELVE_THOUSAND) {
             System.out.println("샴페인 1개");
-        } else if (beforeDiscountPrice < 120000) {
-            System.out.println("없음");
+        } else if (beforeDiscountPrice < TWELVE_THOUSAND) {
+            System.out.println(OutputComment.NOTHING.getOutputComment());
         }
     }
 
     private static void printAfterDiscountPrice(int beforeDiscountPrice, int benefits) {
         int afterDiscountPrice = beforeDiscountPrice - benefits;
         System.out.println();
-        System.out.println("<할인 후 예상 결제 금액>");
-        System.out.println(df.format(afterDiscountPrice) + "원");
+        System.out.println(OutputComment.ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT.getOutputComment());
+        System.out.println(df.format(afterDiscountPrice) + OutputComment.KRW.getOutputComment());
     }
 
 
     private static int calculateChristmasDiscount(int eventDate) {
-        int totalDiscount = 1000;
+        int totalDiscount = THOUSAND;
         if (eventDate >= 26) {
-            return 0;
+            return ZERO;
         }
         for (int day = 1; day < eventDate; day++) {
             totalDiscount += 100;
@@ -100,13 +104,12 @@ public class EventPlanner {
         int discountPerMainDish = 2023;
 
         if (eventDate >= 26) {
-            return 0;
+            return ZERO;
         }
-
         if (dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY) {
             return userOrder.calculateMainDishDiscount(discountPerMainDish);
         }
-        return 0;
+        return ZERO;
     }
 
     private static int calculateWeekdayDiscount(int eventDate, Order userOrder) {
@@ -114,28 +117,28 @@ public class EventPlanner {
         int discountPerDessert = 2023;
 
         if (eventDate >= 26) {
-            return 0;
+            return ZERO;
         }
 
         if (!(dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY)) {
             return userOrder.calculateDessertDiscount(discountPerDessert);
         }
-        return 0;
+        return ZERO;
     }
 
     private static int calculateStarDiscount(int eventDate, DayOfWeek dayOfWeek) {
         LocalDate dateUtils = LocalDate.of(2023, 12, eventDate);
 
-        int discountAmount = 0;
+        int discountAmount = ZERO;
         if (dayOfWeek == DayOfWeek.SUNDAY || dateUtils.getDayOfMonth() == 25) {
-            discountAmount = 1000;
+            discountAmount = THOUSAND;
         }
         return discountAmount;
     }
 
     private static void calculateEventBadge(int totalBenefits) {
         System.out.println();
-        System.out.println("<12월 이벤트 배지>");
+        System.out.println(OutputComment.DECEMBER_EVENT_BADGE.getOutputComment());
         if (totalBenefits >= 20000) {
             System.out.println("산타");
         } else if (totalBenefits >= 10000) {
@@ -143,7 +146,9 @@ public class EventPlanner {
         } else if (totalBenefits >= 5000) {
             System.out.println("별");
         } else if (totalBenefits < 5000) {
-            System.out.println("없음");
+            System.out.println(OutputComment.NOTHING.getOutputComment());
         }
     }
 }
+
+
