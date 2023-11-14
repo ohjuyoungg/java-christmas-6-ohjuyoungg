57eb7fc35e07c7c9e9e55d6f398da9108377aa60
ohjuyoungg <ejrm12@naver.com>
2023-11-15 00:04:08 +0900

    feat: 메뉴 출력 정렬로 인한 LinkedHashMap 으로 변경

--- src/main/java/christmas/service/BeforeEventService.java
+++ src/main/java/christmas/service/BeforeEventService.java
@@ -4,27 +4,30 @@ import christmas.domain.menu.MenuInfo;
 import christmas.domain.order.Order;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class BeforeEventService {
 
+
     public static void printMenu(Order userOrder) {
         List<String> orderedMenu = new ArrayList<>();
 
-        Map<String, Integer> orderItems = userOrder.getOrderItems();
+        LinkedHashMap<String, Integer> orderItems = userOrder.getOrderItems();
         for (Map.Entry<String, Integer> entry : orderItems.entrySet()) {
             String menuName = entry.getKey();
             int quantity = entry.getValue();
 
             orderedMenu.add(String.format("%s %d개", menuName, quantity));
         }
-        Collections.reverse(orderedMenu);
+
         for (String menu : orderedMenu) {
             System.out.println(menu);
         }
     }
+
+
 
     public static int calculateTotalPrice(Order userOrder) {
         return userOrder.getOrderItems()
