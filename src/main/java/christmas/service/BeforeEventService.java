9a2828455d10487684e12748dc6c13cbb707cad7
ohjuyoungg <ejrm12@naver.com>
2023-11-14 15:03:47 +0900

    refactor: 이벤트 전 후로 나누기 위해 클래스명 변경

--- src/main/java/christmas/service/BeforeEventService.java
+++ src/main/java/christmas/service/BeforeEventService.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-public class EventService {
+public class BeforeEventService {
 
     public static void printMenu(Order userOrder) {
         List<String> orderedMenu = new ArrayList<>();
