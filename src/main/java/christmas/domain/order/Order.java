00de92f7f0b9e4d723945c4ba917f8645dc19628
ohjuyoungg <ejrm12@naver.com>
2023-11-15 00:07:04 +0900

    feat: 이벤트 계산으로 인한 디저트 메뉴 및 메인 메뉴 메소드 구현

--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -1,9 +1,15 @@
 package christmas.domain.order;
 
+import static christmas.domain.menu.MenuInfo.isDessert;
+import static christmas.service.BeforeEventService.getMenuPrice;
+
+import christmas.domain.menu.MenuInfo;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Order {
+
     private final Map<String, Integer> orderItems;
 
     public Order() {
@@ -14,7 +20,24 @@ public class Order {
         orderItems.put(menu, quantity);
     }
 
-    public Map<String, Integer> getOrderItems() {
-        return new HashMap<>(orderItems);
+    public LinkedHashMap<String, Integer> getOrderItems() {
+        return new LinkedHashMap<>(orderItems);
+    }
+
+    public int calculateDessertDiscount(int discountPerMenu) {
+        int dessertCount = getOrderItems().entrySet().stream()
+            .filter(entry -> isDessert(entry.getKey()))
+            .mapToInt(Map.Entry::getValue)
+            .sum();
+
+        return dessertCount * discountPerMenu;
+    }
+
+    public int calculateMainDishDiscount(int discountAmount) {
+        return getOrderItems().entrySet().stream()
+            .filter(entry -> !MenuInfo.isDessert(entry.getKey()))
+            .mapToInt(entry -> getMenuPrice(entry.getKey()) * entry.getValue())
+            .map(price -> Math.max(price - discountAmount, 0))
+            .sum();
     }
 }
