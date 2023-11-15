4aa20df80826df9b77e6e4e09dfe59887ba1f337
ohjuyoungg <ejrm12@naver.com>
2023-11-15 18:46:55 +0900

    feat: 할인과 뱃지 상수값 설정

--- src/main/java/christmas/utils/constants/OutputComment.java
+++ src/main/java/christmas/utils/constants/OutputComment.java
@@ -9,7 +9,16 @@ public enum OutputComment {
     ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT("<할인 후 예상 결제 금액>"),
     DECEMBER_EVENT_BADGE("<12월 이벤트 배지>"),
     KRW("원"),
-    NOTHING("없음");
+    NOTHING("없음"),
+    CHRISTMAS_D_DAY_DISCOUNT("크리스마스 디데이 할인: -"),
+    WEEKDAY_DISCOUNT("평일 할인: -"),
+    WEEKEND_DISCOUNT("주말 할인: -"),
+    SPECIAL_DISCOUNT("특별 할인: -"),
+    GIVEAWAY_EVENT("증정 이벤트: -25,000원"),
+    SANTA("산타"),
+    TREE("트리"),
+    STAR("별");
+
 
     private final String outputComment;
 
